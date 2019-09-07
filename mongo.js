560c8138880c1b06a4f5b9da3dbafee5e0f1488b
King-Mike <nsubugamike021@gmail.com>
2019-09-07 17:55:55 +0300

    deploy cred

--- mongo.js
+++ mongo.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose");
 var config = require("dotenv").config();
 module.exports = () => {
   var envUrl = process.env.MONGODB_URI;
-  var localUrl = `mongodb+srv://${config.parsed.username}:${config.parsed.password}@${config.parsed.host}/${config.parsed.database}?retryWrites=true&w=majority`;
+  var localUrl = `mongodb+srv://${process.env.username}:${process.env.password}@${process.env.host}/${process.env.database}?retryWrites=true&w=majority`;
   var mongoUrl = envUrl ? envUrl : localUrl;
   console.log(mongoUrl);
   return mongoose.connect(mongoUrl);
